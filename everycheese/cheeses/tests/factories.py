b7edfe13b615556e346c37e8821c34b10d846536
Alain Serrano <alains1984@gmail.com>
2023-09-22 21:42:39 +0200

    Use CheeseFactory in a test

---
+++ everycheese/cheeses/tests/factories.py
@@ -0,0 +1,15 @@
+from django.template.defaultfilters import slugify
+
+import factory
+import factory.fuzzy
+
+from ..models import Cheese
+
+
+class CheeseFactory(factory.Factory):
+    class Meta:
+        model = Cheese
+    name = factory.fuzzy.FuzzyText()
+    slug = factory.LazyAttribute(lambda obj: slugify(obj.name))
+    description = factory.Faker('paragraph')
+    firmness = factory.fuzzy.FuzzyChoice([x[0] for x in Cheese.Firmness.choices])
--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -1,13 +1,10 @@
 import pytest
 from ..models import Cheese
+from .factories import CheeseFactory
 pytestmark = pytest.mark.django_db
 
 
 def test___str__():
-    cheese = Cheese.objects.create(
-        name="Stracchino",
-        description="Semi-sweet cheese that goes with startches.",
-        firmness=Cheese.Firmness.SOFT
-    )
-    assert cheese.__str__() == "Stracchino"
-    assert str(cheese) == "Stracchino"
+    cheese = CheeseFactory()
+    assert cheese.__str__() == cheese.name
+    assert str(cheese) == cheese.name
