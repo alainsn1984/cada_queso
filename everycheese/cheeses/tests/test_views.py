b2d07e44254b3313269fa2180bafa30309c79015
Alain Serrano <alains1984@gmail.com>
2023-09-24 22:12:55 +0200

    Write tests for all cheese URL patterns

---
+++ everycheese/cheeses/tests/test_views.py
@@ -0,0 +1,69 @@
+import pytest
+from pytest_django.asserts import (
+    assertContains,
+    assertRedirects
+)
+from django.urls import reverse, resolve
+from django.contrib.sessions.middleware import SessionMiddleware
+from django.test import RequestFactory
+
+from everycheese.users.models import User
+from ..models import Cheese
+from ..views import (
+    CheeseCreateView,
+    CheeseListView,
+    CheeseDetailView,
+)
+from .factories import CheeseFactory
+pytestmark = pytest.mark.django_db
+
+
+def test_good_cheese_list_view_expanded(rf:RequestFactory):
+    # Get the request
+    request = rf.get(reverse('cheeses:list'))
+    # Use the request  to get the response
+    response = CheeseListView.as_view()(request)
+    # Test that the response is valid
+    assertContains(response, 'Cheese List')
+
+
+def test_good_cheese_detail_view():
+    """Test the `CheeseDetailView` when route is:
+       /chesses/<name_cheese>/
+    """
+    cheese = CheeseFactory()
+    view = resolve(f'/cheeses/{cheese.slug}/')
+    assert view.func.__name__== CheeseDetailView.as_view().__name__
+
+
+def test_good_cheese_create_view(rf, admin_user):
+    # Order some cheese from the CheeseFactory
+    cheese = CheeseFactory()
+    # Make a request for our new cheese
+    request = rf.get(reverse('cheeses:add'))
+    request.user = admin_user
+    # Use the request  to get the response
+    response = CheeseCreateView.as_view()(request)
+    assert response.status_code == 200
+
+def test_cheese_list_contains_2_cheeses(rf):
+    cheese1 = CheeseFactory()
+    cheese2 = CheeseFactory()
+    request = rf.get(reverse('cheeses:list'))
+    response = CheeseListView.as_view()(request)
+    assert response.status_code == 200
+ 
+
+def test_cheese_create_form_valid(rf:RequestFactory, admin_user):
+    form_data = {
+        'name':'Paski Sir',
+        'description':'A salty hard cheese',
+        'firmness':Cheese.Firmness.HARD
+    }
+    request = rf.post(reverse('cheeses:add'), form_data)
+    request.user = admin_user
+    response = CheeseCreateView.as_view()(request)
+    cheese = Cheese.objects.get(name='Paski Sir')
+    assert cheese.description == 'A salty hard cheese'
+    assert cheese.firmness == Cheese.Firmness.HARD
+    assert cheese.creator == admin_user
